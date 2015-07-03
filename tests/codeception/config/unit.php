10263210d9cbd7cce436bb954359ca2ea36e8be3
Igor Khripun <khripun.igor@gmail.com>
2015-07-03 10:48:54 +0300

    integration phpci

--- tests/codeception/config/unit.php
+++ tests/codeception/config/unit.php
@@ -3,9 +3,9 @@
  * Application configuration for unit tests
  */
 return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../../config/common.php'),
+    require(__DIR__ . '/../../../config/common.local.php'),
     require(__DIR__ . '/../../../config/web.php'),
-    require(__DIR__ . '/config.php'),
-    [
-
-    ]
+    require(__DIR__ . '/../../../config/web.local.php'),
+    require(__DIR__ . '/config.php')
 );
--- tests/codeception/functional/LoginCept.php
+++ tests/codeception/functional/LoginCept.php
@@ -23,6 +23,7 @@ $I->expectTo('see validations errors');
 $I->see('Incorrect username or password.');
 
 $I->amGoingTo('try to login with correct credentials');
-$loginPage->login('admin', 'admin');
+$loginPage->login('admin', '123123');
 $I->expectTo('see user info');
 $I->see('Logout (admin)');
+
