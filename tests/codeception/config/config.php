36ab27f6485adfb5b3fd5f20dbe3d35adde684e8
Igor Khripun <khripun.igor@gmail.com>
2015-07-07 11:53:52 +0300

    change phpci config

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -15,8 +15,8 @@ return [
         'db' => [
             'class'     => 'yii\db\Connection',
             'dsn'       => 'pgsql:host=localhost;dbname=test',
-            'username'  => 'pguser3',
-            'password'  => 'pguser4',
+            'username'  => 'pguser',
+            'password'  => 'pguser',
             'charset'   => 'utf8',
             'schemaMap' => [
                 'pgsql' => [
